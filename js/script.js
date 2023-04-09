1aafa93cfca8a5a104b415b9169a502baf5928cc
omni23 <89713821+omni23@users.noreply.github.com>
2023-04-09 04:00:02 -0400

    Add custom validation messages for inputs

--- js/script.js
+++ js/script.js
@@ -1,19 +1,29 @@
 const pwInput = document.querySelector("#userpw");
 const pwcInput = document.querySelector("#confirmpw");
 const pwcError = document.querySelector("#pwc-error");
+const phoneInput = document.querySelector("#userphone");
 
 function comparePasswords() {
-    if (pwInput.value !== pwcInput.value ) {
-        pwcError.textContent = "Passwords don't match!";
+    if (pwInput.value != pwcInput.value ) {
         pwInput.classList.add("error");
         pwcInput.classList.add("error");
+        pwcInput.setCustomValidity("Passwords don't match");
     }
     else {
-        pwcError.textContent = "";
         pwInput.classList.remove("error");
         pwcInput.classList.remove("error");
+        pwcInput.setCustomValidity("");
     }
 }
 
 pwInput.addEventListener("input", comparePasswords);
 pwcInput.addEventListener("input", comparePasswords);
+phoneInput.addEventListener("input", () => {
+    if (phoneInput.validity.patternMismatch) {
+        phoneInput.setCustomValidity
+        ("Please enter a ten digit phone number");
+    }
+    else {
+        phoneInput.setCustomValidity("");
+    }
+})
